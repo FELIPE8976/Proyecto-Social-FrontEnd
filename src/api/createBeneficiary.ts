9c7dabb6ca298eca8645c590894723ce78815513
SebastianH03 <sebasht2003@gmail.com>
2024-10-01 17:13:02 -0500

    eliminamos token

--- src/api/createBeneficiary.ts
+++ src/api/createBeneficiary.ts
@@ -1,14 +1,12 @@
 const API_URI = process.env.NEXT_PUBLIC_API_URI;
 
 export async function createBeneficiary(form: any) {
-	const token = localStorage.getItem("token");
 	const url = `${API_URI}/dev/users/create-beneficiary`;
 
 	const response = await fetch(url, {
 		method: "POST",
 		headers: {
-			"Content-Type": "application/json",
-			Authorization: `Bearer ${token}`,
+			"Content-Type": "application/json"
 		},
 		body: JSON.stringify(form),
 	});
